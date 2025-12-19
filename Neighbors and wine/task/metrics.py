3846bcdb748160ad3cd2604962b3b3fa89c520ef
Abderrahmane Houri <abderrahmane.houri@ensia.edu.dz>
2025-12-20 00:27:08 +0100

    ff
    ff

--- Neighbors and wine/task/metrics.py
+++ Neighbors and wine/task/metrics.py
@@ -3,24 +3,13 @@ import numpy as np
 
 def precision_recall(y_pred, y_test):
     class_precision_recall = []
-    # Here we calculate the precision and recall for each of the unique classes of the
-    # testing sample.
     for c in np.unique(y_test):
-        # Here we evaluate the number of tp for the class.
-        tp = #TODO
-        # Here we evaluate the number of fp for the class.
-        fp = #TODO
-        # Here we evaluate the number of fn for the class.
-        fn = #TODO
-        # Here we calculate the precision for the class.
-        precision = #TODO
-        # Here we calculate the recall for the class.
-        recall =#TODO
-        # Here we add a tuple containing the class and its precision and recall to the resulting array.
+        tp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] == c])
+        fp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] != c])
+        fn = len([i for i in range(len(y_test)) if y_pred[i] != c and y_test[i] == c])
+        precision = tp / (tp + fp) if tp + fp > 0 else 0.
+        recall = tp / (tp + fn) if tp + fn > 0 else 0.
         class_precision_recall.append((c, precision, recall))
-    # The returned value would be an array of tuples containing the unique classes and the precision/recall
-    # values corresponding to them, like this: [(class 1, precision 1, recall 1), ...
-    # ... , (class n, precision n, recall n)].
     return class_precision_recall
 
 
